a73b22a6d8646c36e72b05fcdb8d36110e119515
a-clap <clapinskiadam@gmail.com>
2022-06-19 10:16:52 +0200

    Modifier testing

--- internal/modifier/modifier.go
+++ internal/modifier/modifier.go
@@ -1,23 +1,71 @@
 package modifier
 
 import (
-	"fmt"
+	"proxier/pkg/logger"
+	"regexp"
 	"strings"
 )
 
 type Modifier struct {
 	lines []string
+	log   logger.Logger
 }
 
-func New(buf []byte) *Modifier {
-	m := &Modifier{lines: strings.Split(string(buf), "\n")}
+type lineMatcher interface {
+	Match(s string) bool
+}
+
+func New(buf []byte, logger logger.Logger) *Modifier {
+	m := &Modifier{
+		lines: strings.Split(string(buf), "\n"),
+		log:   logger,
+	}
 	return m
 }
 
-func (m *Modifier) RemoveLines() error {
-	return fmt.Errorf("not implemented yet")
+func (m *Modifier) RemoveLines(pattern string) (linesRemoved int, err error) {
+	r, err := regexp.Compile(pattern)
+	var lm lineMatcher
+	if err != nil {
+		m.log.Infof("Pattern not compiled, trying with exact string")
+		lm = &regexMatcher{r}
+	} else {
+		lm = &stringsMatcher{p: pattern}
+	}
+
+	return m.removeLines(lm)
+}
+
+func (m *Modifier) removeLines(lm lineMatcher) (linesRemoved int, err error) {
+	var removedLines []int
+
+	for i, line := range m.lines {
+		if ok := lm.Match(line); ok {
+			removedLines = append(removedLines, i)
+		}
+	}
+	for i, lineNumber := range removedLines {
+		m.lines = append(m.lines[:lineNumber-i], m.lines[lineNumber-i+1:]...)
+	}
+	linesRemoved = len(removedLines)
+	return
 }
 
 func (m *Modifier) Get() []byte {
 	return []byte(strings.Join(m.lines, "\n"))
+}
+
+type regexMatcher struct {
+	*regexp.Regexp
+}
+
+type stringsMatcher struct {
+	p string
+}
+
+func (r *regexMatcher) Match(s string) bool {
+	return r.Match(s)
+}
+func (sm *stringsMatcher) Match(s string) bool {
+	return strings.Contains(s, sm.p)
 }
--- internal/modifier/modifier_test.go
+++ internal/modifier/modifier_test.go
@@ -2,6 +2,7 @@ package modifier_test
 
 import (
 	"proxier/internal/modifier"
+	"proxier/pkg/logger"
 	"reflect"
 	"testing"
 )
@@ -33,9 +34,67 @@ func TestNew_Get(t *testing.T) {
 	}
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
-			m := modifier.New(tt.args.buf)
+			m := modifier.New(tt.args.buf, logger.Dummy{})
 			if got := m.Get(); !reflect.DeepEqual(got, tt.args.buf) {
 				t.Errorf("New() = %v, want %v", got, tt.args.buf)
+			}
+		})
+	}
+}
+
+func TestModifier_RemoveLines(t *testing.T) {
+	type fields struct {
+		buf []byte
+	}
+	type args struct {
+		pattern string
+	}
+	tests := []struct {
+		name             string
+		fields           fields
+		args             args
+		wantLinesRemoved int
+		wantErr          bool
+		wantBuf          fields
+	}{
+		{
+			name:             "Remove one line with new line",
+			fields:           fields{buf: []byte("keep this line\nremove this line\n")},
+			args:             args{pattern: `remove this line`},
+			wantLinesRemoved: 1,
+			wantErr:          false,
+			wantBuf:          fields{buf: []byte("keep this line\n")},
+		},
+		{
+			name:             "Remove two lines",
+			fields:           fields{buf: []byte("keep this line\nremove this line\nremove this line\n")},
+			args:             args{pattern: `remove this line`},
+			wantLinesRemoved: 2,
+			wantErr:          false,
+			wantBuf:          fields{buf: []byte("keep this line\n")},
+		},
+		{
+			name:             "Remove many lines",
+			fields:           fields{buf: []byte("keep this line\nremove this line\nremove this line\nremove this line\nremove this line\nremove this line\nremove this line\nremove this line\n")},
+			args:             args{pattern: `remove this line`},
+			wantLinesRemoved: 7,
+			wantErr:          false,
+			wantBuf:          fields{buf: []byte("keep this line\n")},
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			m := modifier.New(tt.fields.buf, logger.Dummy{})
+			gotLinesRemoved, err := m.RemoveLines(tt.args.pattern)
+			if (err != nil) != tt.wantErr {
+				t.Errorf("RemoveLines() error = %v, wantErr %v", err, tt.wantErr)
+				return
+			}
+			if gotLinesRemoved != tt.wantLinesRemoved {
+				t.Errorf("RemoveLines() gotLinesRemoved = %v, want %v", gotLinesRemoved, tt.wantLinesRemoved)
+			}
+			if got := m.Get(); !reflect.DeepEqual(got, tt.wantBuf.buf) {
+				t.Errorf("RemoveLines() gotBuf = %s\n, want %s\n", string(got), string(tt.wantBuf.buf))
 			}
 		})
 	}
